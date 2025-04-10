6dcbd1c20928b9a20c237fdcdb8df8eaf84fbbf5
Sebastian <winigossos@gmail.com>
2025-04-10 18:46:37 -0300

    feat: connect veterinarianControllers.ts with mongoDB

--- backend/controllers/veterinarioControllers.ts
+++ backend/controllers/veterinarioControllers.ts
@@ -1,7 +1,17 @@
-export const register = (req, res) => {
-  const { email, password, nombre } = req.body;
+import Veterinario from "../models/veterinarian.ts";
+export const register = async(req, res) => {
+  // const { email, password, nombre } = req.body;
 
-  res.json({ msg: "Registrando Usuario" });
+  try {
+    // Save new veterinarian
+    const veterinario = new Veterinario(req.body);
+    const veterinarioGuardado = await veterinario.save();
+
+    res.json(veterinarioGuardado);
+  } catch (error) {
+    console.log( error )
+  }
+
 };
 
 export const profil = (req, res) => {
