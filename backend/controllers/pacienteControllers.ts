426329307e4ab8ef2e44142430b61e8a3258089d
Sebastian <winigossos@gmail.com>
2025-04-15 09:48:25 -0300

    feat: update paciente

--- backend/controllers/pacienteControllers.ts
+++ backend/controllers/pacienteControllers.ts
@@ -1,5 +1,5 @@
 import Paciente from "../models/Paciente.ts";
-import Veterinario from '../models/Veterinarian';
+import Veterinario from "../models/Veterinarian";
 
 export const addPaciente = async (req, res) => {
   const newPaciente = req.body;
@@ -23,16 +23,40 @@ export const getPacientes = async (req, res) => {
 
 export const getPaciente = async (req, res) => {
   const { id } = req.params;
-  const paciente = await Paciente.findById(id)
-  if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
-    return res.json({msg: 'Accion no valida'})
+  const paciente = await Paciente.findById(id);
+
+  if (!paciente) return res.status(404).json({ msg: "not found Paciente or Veterinario" });
+  
+  if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+    return res.json({ msg: "Accion no valida" });
   }
-  if(paciente){
-    res.json(paciente )
-  }
+
+  return res.json(paciente);
 };
 
 export const udpatePaciente = async (req, res) => {
+  const { id } = req.params;
+  const paciente = await Paciente.findById(id);
+
+  if (!paciente) return res.status(404).json({ msg: "not found Paciente or Veterinario" });
+
+  if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+    return res.json({ msg: "Accion no valida" });
+  }
+
+  // update Paciente
+  paciente.nombre = req.body.nombre || paciente.nombre
+  paciente.propietario = req.body.propietario || paciente.propietario
+  paciente.email = req.body.email || paciente.email
+  paciente.fecha = req.body.fecha || paciente.fecha
+  paciente.sintomas = req.body.sintomas || paciente.sintomas
+
+  try {
+    const updatedPacient = await paciente.save()
+    res.json( updatedPacient )
+  } catch (error) {
+    console.log(error)
+  }
 
 };
 
