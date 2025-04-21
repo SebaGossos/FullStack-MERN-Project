cd2ea5bdac02a7190b699fd027d4b479051f74e5
Sebastian <winigossos@gmail.com>
2025-04-21 08:42:34 -0300

    feat: adding classes and inputs to form with link

--- fronted/src/pages/Login.tsx
+++ fronted/src/pages/Login.tsx
@@ -1,23 +1,39 @@
+import { Link } from "react-router-dom";
 function Login() {
   return (
     <>
       <div>
-        <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión y Administra tus <span className="text-black">Pacientes</span></h1>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Inicia Sesión y Administra tus <span className="text-black">Pacientes</span>
+        </h1>
       </div>
       <div>
         <form action="">
           <div className="my-5">
-            <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">Email</label>
-            <input type="email" placeholder="Email de Registro" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"/>
+            <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">
+              Email
+            </label>
+            <input type="email" placeholder="Email de Registro" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
           </div>
 
           <div className="my-5">
-            <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">Password</label>
-            <input type="password" placeholder="Tu password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"/>
+            <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">
+              Password
+            </label>
+            <input type="password" placeholder="Tu password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
           </div>
 
-          <input type="submit" value="Iniciar Sesión" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto "/>
+          <input type="submit" value="Iniciar Sesión" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto " />
         </form>
+
+        <nav className="mt-10 md:flex justify-between">
+          <Link className="block text-center my-5 text-gray-500" to="/registrar">
+            ¿No tienes una cuenta? Registrate
+          </Link>
+          <Link className="block text-center my-5 text-gray-500" to="/registrar">
+            Olvide Mi Password
+          </Link>
+        </nav>
       </div>
     </>
   );
