b9e8c7cff132d7e46afc4367659f166ea7ac481a
Sebastian <winigossos@gmail.com>
2025-04-22 08:43:17 -0300

    feat: validacion del formulario

--- fronted/src/pages/Registrar.tsx
+++ fronted/src/pages/Registrar.tsx
@@ -6,6 +6,23 @@ const Registrar = () => {
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if ([nombre, email, password, password2].includes("")) {
+      console.log("Hay al menos un campo vacío");
+      return;
+    }
+    if (password !== password2) {
+      console.log("Los passwords no son iguales");
+      return;
+    }
+    if (password.length < 6) {
+      console.log("El password es muy corto, agrega mínimo 6 caracteres");
+      return;
+    }
+    console.log("Todo bien, enviar formulario");
+  };
+
   return (
     <>
       <div>
@@ -14,33 +31,33 @@ const Registrar = () => {
         </h1>
       </div>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">
               Nombre
             </label>
-            <input type="email" placeholder="Tu nombre" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={nombre} onChange={ e => setNombre(e.target.value)} />
+            <input type="text" placeholder="Tu nombre" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={nombre} onChange={(e) => setNombre(e.target.value)} />
           </div>
 
           <div className="my-5">
-            <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="" >
+            <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">
               Email
             </label>
-            <input type="email" placeholder="Email de Registro" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={email} onChange={ e => setEmail(e.target.value)}/>
+            <input type="email" placeholder="Email de Registro" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
 
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">
               Password
             </label>
-            <input type="password" placeholder="Tu password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={password} onChange={ e => setPassword(e.target.value)}/>
+            <input type="password" placeholder="Tu password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
 
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">
               Repetir Password
             </label>
-            <input type="password" placeholder="Repite tu password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"  value={password2} onChange={ e => setPassword2(e.target.value)}/>
+            <input type="password" placeholder="Repite tu password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={password2} onChange={(e) => setPassword2(e.target.value)} />
           </div>
 
           <input type="submit" value="Crear Cuenta" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto " />
