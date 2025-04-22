fe10045991a36e83566242e700e234de0eb73e74
Sebastian <winigossos@gmail.com>
2025-04-22 08:12:38 -0300

    feat: create Register

--- fronted/src/pages/Login.tsx
+++ fronted/src/pages/Login.tsx
@@ -7,8 +7,15 @@ function Login() {
           Inicia Sesión y Administra tus <span className="text-black">Pacientes</span>
         </h1>
       </div>
-      <div>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
         <form action="">
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">
+              Nombre 
+            </label>
+            <input type="email" placeholder="Tu nombre" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+          </div>
+
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold" htmlFor="">
               Email
