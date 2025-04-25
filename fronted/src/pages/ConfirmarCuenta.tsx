b0bff504dae7a6078b7cff8c1153908f70d865bc
Sebastian <winigossos@gmail.com>
2025-04-25 09:53:34 -0300

    feat(fronted/pages/confirmarCuenta.ts): finish confirm user with fetch and efect from react

--- fronted/src/pages/ConfirmarCuenta.tsx
+++ fronted/src/pages/ConfirmarCuenta.tsx
@@ -1,9 +1,66 @@
+import { useEffect, useRef, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import Alerta from "../components/Alerta.tsx";
 const ConfirmarCuenta = () => {
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+  const [cargando, setCargando] = useState(true);
+  const [alerta, setAlerta] = useState({});
+
+  const { token } = useParams();
+
+  // Evita doble petición en StrictMode
+  const peticionHecha = useRef(false);
+
+  useEffect(() => {
+    if (!token || peticionHecha.current) return;
+    peticionHecha.current = true;
+    const confirmarCuenta = async () => {
+      try {
+        const url = `http://localhost:4000/api/veterinarios/confirmar/${token}`;
+        const respuesta = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await respuesta.json();
+
+        if (!respuesta.ok) {
+          throw new Error(data.msg || "Error en la confirmación de la cuenta");
+        }
+        setCuentaConfirmada(true);
+        setAlerta({ msg: data.msg });
+        console.log(data);
+      } catch (error) {
+        setAlerta({ msg: error.message, error });
+        console.error("Error al confirmar la cuenta:", error);
+        // Manejar el error, como mostrar un mensaje de error al usuario
+      }
+      setCargando(false);
+    };
+    confirmarCuenta();
+  }, [token]);
+
   return (
     <>
-      <h1>Desde ConfirmarCuenta</h1>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Confirma tu cuenta y administra
+          <span className="text-black"> tus Pacientes</span>
+        </h1>
+      </div>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        {!cargando && <Alerta alerta={alerta} />}
+        {cuentaConfirmada && (
+          <nav className="mt-10 md:flex justify-between">
+            <Link className="block text-center my-5 text-gray-500" to="/">
+              Inicia Sesión
+            </Link>
+          </nav>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ConfirmarCuenta
+export default ConfirmarCuenta;
