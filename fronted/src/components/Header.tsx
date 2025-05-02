6c52512c2829b6ca0d04fc14b2614206f8b813d0
Sebastian <winigossos@gmail.com>
2025-05-02 08:31:43 -0300

    feat: enhance Header layout for better responsiveness and update RutaProtegida structure for improved readability

--- fronted/src/components/Header.tsx
+++ fronted/src/components/Header.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="p-10 bg-indigo-600 ">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="font-bold text-2xl text-indigo-200">
+      <div className="container mx-auto flex justify-between items-center flex-col lg:flex-row">
+        <h1 className="font-bold text-2xl text-indigo-200 text-center">
           {" "}
           Administrador de Pacientes de {""} <span className="text-white font-black">Veterinaria</span>
         </h1>
-        <nav className="flex gap-4">
+        <nav className="flex gap-4 flex-col items-center lg:flex-row mt-5 lg:mt-0">
           <Link to='/admin' className="text-white text-sm uppercase font-bold">Pacientes</Link>
           <Link to='/perfil' className="text-white text-sm uppercase font-bold">Perfil</Link>
 
--- fronted/src/layout/RutaProtegida.tsx
+++ fronted/src/layout/RutaProtegida.tsx
@@ -13,7 +13,11 @@ function RutaProtegida() {
   return (
     <>
       <Header />
-      { auth?._id ? <Outlet /> : <Navigate to="/" /> }
+      { auth?._id ? (
+        <main className="container mx-auto mt-10">
+          <Outlet />
+        </main>
+      ) : <Navigate to="/" /> }
       <Footer />
     </>
   );
