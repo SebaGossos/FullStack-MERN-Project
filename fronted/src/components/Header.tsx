1c3961f8e8a74cfa7e01c9dcae920e8e06d88082
Sebastian <winigossos@gmail.com>
2025-05-02 07:58:36 -0300

    feat: implement Header component with navigation links and session button

--- fronted/src/components/Header.tsx
+++ fronted/src/components/Header.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <h1>Header</h1>
-  )
+    <header className="p-10 bg-indigo-600 ">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="font-bold text-2xl text-indigo-200">
+          {" "}
+          Administrador de Pacientes de {""} <span className="text-white font-black">Veterinaria</span>
+        </h1>
+        <nav className="flex gap-4">
+          <Link to='/admin' className="text-white text-sm uppercase font-bold">Pacientes</Link>
+          <Link to='/perfil' className="text-white text-sm uppercase font-bold">Perfil</Link>
+
+          <button type="button" className="text-white text-sm uppercase font-bold" >
+            Cerrar Sesion
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
