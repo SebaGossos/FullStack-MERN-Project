ed3d5a770a138513e9c6d3e16ee78349497a3f92
Sebastian <winigossos@gmail.com>
2025-05-04 09:18:41 -0300

    feat: update ListadoPacientes component to display patient list or empty state

--- fronted/src/components/ListadoPacientes.tsx
+++ fronted/src/components/ListadoPacientes.tsx
@@ -1,7 +1,28 @@
+import usePacientes from "../hooks/usePacientes.tsx";
 function ListadoPacientes() {
+  const { pacientes } = usePacientes();
+  console.log(pacientes);
   return (
-    <h1>ListadoPacientes</h1>
-  )
+    <>
+      {pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+          </p>
+        </>
+      )}
+    </>
+  );
 }
 
-export default ListadoPacientes
+export default ListadoPacientes;
