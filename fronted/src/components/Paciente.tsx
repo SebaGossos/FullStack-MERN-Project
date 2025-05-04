326acdf1e54f142a7b2011a8d40f3f4a7e0c4d09
Sebastian <winigossos@gmail.com>
2025-05-04 09:57:33 -0300

    feat: add edit and delet btn in component Paciente

--- fronted/src/components/Paciente.tsx
+++ fronted/src/components/Paciente.tsx
@@ -1,11 +1,48 @@
 const Paciente = ({ paciente }) => {
   const { email, fecha, nombre, propietario, sintomas, _id } = paciente;
+
+  console.log( fecha )
+
+  const formatearFecha = ( fecha ) => {
+    const nuevaFecha = new Date(fecha)
+    return new Intl.DateTimeFormat('es-mx',{dateStyle:'long'}).format(nuevaFecha)
+  }
+
+  console.log(fecha)
+  
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
-      <p className="font-bold uppercase text-gray-500">
+      <p className="font-bold uppercase text-gray-800 my-2">
         Nombre: {''}
         <span className="font-normal normal-case text-black">{nombre}</span>
       </p>
+      
+      <p className="font-bold uppercase text-gray-800 my-2">
+        Propietario: {''}
+        <span className="font-normal normal-case text-black">{propietario}</span>
+      </p>
+      
+      <p className="font-bold uppercase text-gray-800 my-2">
+        Email: {''}
+        <span className="font-normal normal-case text-black">{email}</span>
+      </p>
+      
+      <p className="font-bold uppercase text-gray-800 my-2">
+        Fecha de Alta: {''}
+        <span className="font-normal normal-case text-black">{formatearFecha(fecha)}</span>
+      </p>
+      
+      <p className="font-bold uppercase text-gray-800 my-2">
+        Síntomas: {''}
+        <span className="font-normal normal-case text-black">{sintomas}</span>
+      </p>
+
+      <div className="flex justify-between mt-5">
+        <button type="button" className="py-2 px-2 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg">Editar</button>
+
+        <button type="button" className="py-2 px-2 bg-red-600 hover:bg-red-700 text-white uppercase font-bold rounded-lg">Eliminar</button>
+      </div>
+
     </div>
   );
 };
